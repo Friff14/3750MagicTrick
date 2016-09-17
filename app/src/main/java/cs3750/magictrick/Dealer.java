aaa41a940012389e5712b1d4ca53ad72c16174c7
bomber916 <jaredwilliams@mail.weber.edu>
2016-09-17 01:04:40 -0600

    Started work on Dealer

--- app/src/main/java/cs3750/magictrick/Dealer.java
+++ app/src/main/java/cs3750/magictrick/Dealer.java
@@ -1,18 +1,49 @@
 package cs3750.magictrick;
 
+import java.util.Stack;
+
 /**
  * Created by sethalumps on 9/14/2016.
  */
 public class Dealer {
     private int dealNumber;
+    private Stack<Card> pickedUpCards;
+    private Deck deck;
+    private Board board;
 
-    public void deal(){
+    public Dealer(){
+        dealNumber = 0;
+        pickedUpCards = new Stack<>();
+        board = new Board();
+        deck = new Deck();
+        deck.shuffle();
 
+        for (Card card : deck.random21())
+        {
+            pickedUpCards.push(card);
+        }
+    }
+
+    public Column[] deal()
+    {
+        
+        int i = 0;
+        while (!pickedUpCards.isEmpty())
+        {
+            board.addToColumn(i,pickedUpCards.pop());
+            i++;
+
+            if (i>=3)
+            {
+                i=0;
+            }
+        }
     }
     public void revealCard(){
 
     }
     public void pickUpCard(){
-
+        Player player = new Player();
+        Board
     }
 }
