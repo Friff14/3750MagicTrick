86eee1b2027d9aba6022e41900e3e71a2f4099c1
Robbie <robbiextreme@gmail.com>
2016-09-16 20:35:14 -0600

    Added initial functionality. Nothing much.

--- app/src/main/java/cs3750/magictrick/Player.java
+++ app/src/main/java/cs3750/magictrick/Player.java
@@ -1,15 +1,40 @@
 package cs3750.magictrick;
 
+import android.app.Activity;
+
 /**
  * Created by sethalumps on 9/14/2016.
  */
-public class Player {
+public class Player
+{
+    //flag that indicates that a card has been chosen.
     private boolean hasSelectedCard;
 
-    public void indicateColumn(){
 
+    /**
+     * Constructor. sets hasSelectedCard to false;
+     */
+    public Player()
+    {
+        hasSelectedCard = false;
     }
-    public void pickCard(){
 
+    /**
+     * Indicates a column
+     */
+    public void indicateColumn(int column)
+    {
+        /*TODO Prompt a user somewhere to choose a column?
+        maybe we should have a message area that's always on the screen, rather than taking up space with a dialog box.
+         */
+    }
+
+    /**
+     * Indicates that a card has been selected
+     */
+    public void pickCard()
+    {
+        //TODO probably more stuff.
+        hasSelectedCard = true;
     }
 }
